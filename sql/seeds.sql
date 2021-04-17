ce5fc459f5a832bdc6a235a4d2579cfec619786c
Ryan Helm <rjhelm@github.io>
2021-04-16 21:09:24 -0600

    started adding some data in seeds.sql

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,2 +1,34 @@
 USE employee_db;
 
+/* department array */
+INSERT INTO department (name)
+VALUE ("Legal");
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+
+/* role data array */
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 300000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 125000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 50000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 110000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 160000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 105000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Account Manager", 165000, 4);
+
+/* employees data array */
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+Value()
+
